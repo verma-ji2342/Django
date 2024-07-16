a837e083f8265ff74eff440090c180d318d110f7
Pranjal <pranjal2342.be21@chitkara.edu.in>
2024-07-16 16:33:29 +0530

    till put fetch request

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,7 @@
 urls.py
 """
 # pylint: disable-all
-from home.views import index, person 
+from home.views import index, person, edit_person 
 from django.urls import path
 
 
@@ -10,5 +10,6 @@ from django.urls import path
 
 urlpatterns = [
     path('index/', index),
-    path('person/', person)
+    path('person/', person),
+    path('edit_person/', edit_person)
 ]
--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,11 @@
+"""
+Admin routes
+"""
+
 from django.contrib import admin
+from home.models import Person
 
 # Register your models here.
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'age']
