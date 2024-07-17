d941960bb9e7a20f92be8739ee2a45ef10df8114
Pranjal <pranjal2342.be21@chitkara.edu.in>
2024-07-17 10:43:30 +0530

    modification

--- home/serializer.py
+++ home/serializer.py
@@ -7,7 +7,24 @@ from .models import Person
 
 
 class PeopleSerializer(serializers.ModelSerializer):
-    
+
+    country = serializers.SerializerMethodField()
     class Meta:
         model = Person
         fields = '__all__'
+
+    def get_country(self, obj):
+        return "India"
+
+    def validate_age(self, age):
+        if age < 18:
+            raise serializers.ValidationError('age should be greator than 18')
+        
+        return age
+    
+    def validate_name(self, name):
+        special_characters = "!@#$%^&*()_+|"
+        print("data", name)
+        if any (c in special_characters for c in name):
+            raise serializers.ValidationError('Name should not contain any special character')        
+        return name
